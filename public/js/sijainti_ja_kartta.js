81271b3171d803fc698bd282c0ed10097b20f966
unknown <koodariari14@gmail.com>
2019-09-26 18:29:12 +0300

    kartta lisätty

--- public/js/sijainti_ja_kartta.js
+++ public/js/sijainti_ja_kartta.js
@@ -2,12 +2,24 @@ if ("geolocation" in navigator) {
   console.log("Sijainti saatavilla");
   navigator.geolocation.getCurrentPosition(function(position){
     console.log(position);
+
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
+
     console.log(latitude)
     console.log(longitude)
-  }
+
+    document.getElementById("leveysasteet").innerHTML=latitude;
+    document.getElementById("pituusasteet").innerHTML=longitude;
+
+var kartta = L.map('kartta').setView([latitude, longitude], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(kartta);
+
+  });
 }
 else{
-  console.log("Sijainti ei saatavilla");
+  console.log("Sijaintitietoa ei saatavilla");
 }
